52bfe138d90c7b33d8917e57853c1b88d1731d4d
bsd0x <root@bsd0x.me>
2020-08-01 12:21:56 -0300

    Add elasticip analyzer

--- awsreport/core/analyzer.py
+++ awsreport/core/analyzer.py
@@ -1,5 +1,7 @@
 from awsresources.ami import AmiAnalyzer
 from awsresources.securitygroup import SgAnalyzer
+from awsresources.elasticip import ElasticIpAnalyzer
+
 from core.argument_parser import CliArgumentParser
 from core.log import Logging
 
@@ -16,3 +18,6 @@ class Analyzer(Logging):
                 return SgAnalyzer(args.cidr).find_security_group_by_rule()
 
             return SgAnalyzer().find_security_group_by_rule()
+
+        if args.elasticip:
+            return ElasticIpAnalyzer().find_elastic_dissociated()
