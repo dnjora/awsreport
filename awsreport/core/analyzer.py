32bae4896e56a79a7222acf967233196c0890e02
bsd0x <root@bsd0x.me>
2020-08-01 12:11:00 -0300

    Create class Analyzer

---
+++ awsreport/core/analyzer.py
@@ -0,0 +1,18 @@
+from awsresources.ami import AmiAnalyzer
+from awsresources.securitygroup import SgAnalyzer
+from core.argument_parser import CliArgumentParser
+from core.log import Logging
+
+class Analyzer(Logging):
+    def __init__(self):
+        self.log = Logging()
+
+    def aws_scan(self, args):
+        if args.ami and args.owner:
+            return AmiAnalyzer().find_public_ami(owners=[args.owner])
+
+        if args.sg:
+            if args.cidr:
+                return SgAnalyzer(args.cidr).find_security_group_by_rule()
+
+            return SgAnalyzer().find_security_group_by_rule()
