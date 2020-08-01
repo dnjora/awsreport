1c051fec670024c76f3775872b3105472fda0df4
bsd0x <root@bsd0x.me>
2020-08-01 12:21:35 -0300

    Remove help and add flag elasticip

--- awsreport/core/argument_parser.py
+++ awsreport/core/argument_parser.py
@@ -6,20 +6,20 @@ class CliArgumentParser():
 
         parser.add_argument('--ami', '--images',
                             action="store_true",
-                            help='Analyze AWS AMI (Images)',
                             required=False)
 
         parser.add_argument('--sg', '--securitygroup',
                             action="store_true",
-                            help='Analyze security groups',
                             required=False)
 
         parser.add_argument('--cidr',
-                            help='Network cidr',
                             required=False)
 
         parser.add_argument('--owner', '--imagesowner',
-                            help="Set the owner of the AMI's",
+                            required=False)
+
+        parser.add_argument('--elasticip',
+                            action="store_true",
                             required=False)
 
         args = parser.parse_args()
